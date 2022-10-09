d922546c9fc8480c9fbb2a7ef991b5cfc2565598
Hardik <hardikg2907@gmail.com>
2022-10-09 09:18:41 +0530

    added dictionary checking

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const word = "BOMBZ";
+// variables
+const word = "BOMBS";
 let wordin = '';
 let j=0;
 const boxRows = document.querySelectorAll('.boxes-row');
 let boxes = boxRows[j].querySelectorAll('.boxes');
-const errorBox = document.querySelector('.error');
-
-console.log(boxes); 
+const errorBox = document.querySelectorAll('.error');
 const buttons = document.querySelectorAll('.key');
-
 let count = 0; //count of the number of boxes
+let stat; // status of the api request
+let data =''; 
+let requestUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
+let victory=0;
 
-function printing(letter){
+// backend
+
+async function makeRequest(requestUrl){
+    const response = await fetch(requestUrl);
+    data = await response.json();
+    stat = response.status;
+    if(!response.ok){
+        console.log('Failed to fetch :(', stat);
+    }
+    else console.log('Victory!!!!', stat);
+};
+
+async function printing(letter){
     letter = letter.toUpperCase();
     // handling different events
     if(letter>='A' && letter<='Z'){
@@ -21,16 +35,36 @@ function printing(letter){
             boxes[count].classList.remove('boxes-anim');
         }
         else if(letter==='ENTER'){
-            err = error();
+            let err = error(0);
             if(err==1){
-                check();
+                requestUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordin}`;
+                // console.log(requestUrl);
+                await makeRequest(requestUrl);
+                // console.log(data);
+                if(stat==200)
+                {
+                    // console.log(stat);
+                    // console.log(data);
+                    if(word===wordin){
+                        victory = 1;
+                        anim('boxes-vict');
+                    }
+                    else anim('boxes-rotate');
+                    check();
+                    wordin='';
+                    setTimeout(()=>{keychange();},3000);
+                    count=0;
+                }                
+                else{
+                    // console.log(stat);
+                    boxRows[j].classList.add('boxes-row-shake');
+                    errorBox[1].classList.add('error-show');
+                    setTimeout(()=>{
+                        boxRows[j].classList.remove('boxes-row-shake');
+                        errorBox[1].classList.remove('error-show');
+                    }, 750);
+                }
                 
-                wordin='';
-                rotating();
-                setTimeout(()=>{keychange();
-                console.log(120);},3000);
-                console.log('j='+j);
-                count=0;
             }
         }
         else{
@@ -43,26 +77,26 @@ function printing(letter){
     }
 }
 
-function rotating(){
+function anim(animName){
+    console.log(animName);
     for(let i=0;i<6;i++){
         setTimeout(()=>{
-            if(i==5) j++;
+            if(i==5 && j!=5) j++;
             else{
-                boxRows[j].querySelectorAll('.boxes')[i].classList.add('boxes-rotate');
+                boxRows[j].querySelectorAll('.boxes')[i].classList.add(animName);
             }
             boxes = boxRows[j].querySelectorAll('.boxes');
         }, i==5?i*200:i*250);
     }
 }
 
-
-function error(){
+function error(ind){
     if(count<5){
         boxRows[j].classList.add('boxes-row-shake');
-        errorBox.classList.add('error-show');
+        errorBox[ind].classList.add('error-show');
         setTimeout(()=>{
             boxRows[j].classList.remove('boxes-row-shake');
-            errorBox.classList.remove('error-show');
+            errorBox[ind].classList.remove('error-show');
         }, 750);
         return 0;
     }
@@ -70,9 +104,11 @@ function error(){
 }
 
 function check(){
+    let copy=word;
     for(var i=0;i<5;i++){
-        for(var j=0;j<wordin.length;j++){
-            if(word[j]===wordin[i]){
+        for(var j=0;j<5;j++){
+            if(copy[j]===wordin[i]){
+                copy=copy.substring(0,j)+'1'+copy.substring(j+1);
                 if(i==j){
                     boxes[i].classList.add('green');
                     keychange(boxes[i].textContent,'green');
@@ -80,11 +116,18 @@ function check(){
                 else{
                     boxes[i].classList.add('yellow');
                     keychange(boxes[i].textContent,'yellow');
-                };
+                }
+                break;
             }
             else{boxes[i].classList.add('grey');
             keychange(boxes[i].textContent,'grey');}
         }
+        // console.log("i"+i);
+        if(i==5){
+            alert("bas bhai hogaya tera chance");
+            break;
+        }
+        
     }
 }
 
@@ -115,3 +158,5 @@ document.addEventListener('keypress',(event)=>{
     // console.log(count);
     printing(event.key);
 })
+
+console.log('It\'s working!');
